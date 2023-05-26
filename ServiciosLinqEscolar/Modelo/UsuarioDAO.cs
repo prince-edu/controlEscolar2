34894e208b8f3aabe7be2db954644e0ef661aa29
Edu <Edu@EDU>
2023-05-26 17:47:24 -0600

    Agregar archivos de proyecto.

---
+++ ServiciosLinqEscolar/Modelo/UsuarioDAO.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiciosLinqEscolar.Modelo
+{
+    public static class UsuarioDAO
+    {
+        public static List<Usuario> obtenerUsuarios()
+        {
+            DataClassesEscolarUVDataContext conexionBD = getConnection();
+
+            IQueryable < Usuario > usuario = from usuarioQuery in conexionBD.Usuario select usuarioQuery;
+
+            return usuario.ToList();
+        }
+
+
+        public static Usuario iniciarSesion(string username, string password)
+        {
+            try
+            {
+                DataClassesEscolarUVDataContext conexionBD = getConnection();
+
+                var usuario = (from userlogin in conexionBD.Usuario
+                               where userlogin.username == username &&
+                               userlogin.password == password
+                               select userlogin).FirstOrDefault();
+                return usuario;
+
+            } catch
+            {
+                return null;
+            }
+        }
+
+        public static Boolean guardarUsuario(Usuario usuarioNuevo)
+        {
+            try
+            {
+                DataClassesEscolarUVDataContext conexionBD = getConnection();
+
+                var usuario = new Usuario()
+                {
+                    nombre = usuarioNuevo.nombre,
+                    apellidoPaterno = usuarioNuevo.apellidoPaterno,
+                    apellidoMaterno = usuarioNuevo.apellidoMaterno,
+                    username = usuarioNuevo.username,
+                    password = usuarioNuevo.password
+                };
+
+                conexionBD.Usuario.InsertOnSubmit(usuario);
+                conexionBD.SubmitChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+
+        public static Boolean editarUsuario(Usuario usuarioEdicion)
+        {
+            try
+            {
+                DataClassesEscolarUVDataContext conexionBD = getConnection();
+
+                var usuario = (from UsuarioEdicion in conexionBD.Usuario
+                               where usuarioEdicion.idUsuario == usuarioEdicion.idUsuario
+                               select usuarioEdicion).FirstOrDefault();
+
+                if (usuario != null)
+                {
+                    usuario.nombre = usuarioEdicion.nombre;
+                    usuario.apellidoPaterno = usuarioEdicion.apellidoPaterno;
+                    usuario.apellidoPaterno = usuarioEdicion.apellidoMaterno;
+                    usuario.password = usuarioEdicion.password;
+                    conexionBD.SubmitChanges();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+
+        public static Boolean eliminarUsuario(int idUsuario)
+        {
+            try
+            {
+                DataClassesEscolarUVDataContext conexionBD = getConnection();
+                Usuario UsuarioEliminar = (from usuario in conexionBD.Usuario
+                                           where usuario.idUsuario == idUsuario
+                                           select usuario).FirstOrDefault();
+
+                if(UsuarioEliminar != null)
+                {
+                    conexionBD.Usuario.DeleteOnSubmit(UsuarioEliminar);
+                    conexionBD.SubmitChanges();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public static DataClassesEscolarUVDataContext getConnection()
+        {
+            return new DataClassesEscolarUVDataContext(global::System.Configuration.ConfigurationManager.ConnectionStrings["escolarUVConnectionString"].ConnectionString);
+
+        }
+    }
+    
+}
---
+++ ServiciosLinqEscolar/Service1.svc.cs
@@ -0,0 +1,48 @@
+﻿using ServiciosLinqEscolar.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ServiciosLinqEscolar
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+       public List<Usuario> ObtenerUsuarios()
+        {
+            return UsuarioDAO.obtenerUsuarios();
+        }
+
+        public Boolean GuardarUsuario(Usuario usuarioNuevo)
+        {
+            if(UsuarioDAO.guardarUsuario(usuarioNuevo) == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Usuario IniciarSesion(string username, string password) 
+        {
+            return UsuarioDAO.iniciarSesion(username, password);
+        }
+
+        public Boolean EditarUsuario(Usuario usuario)
+        {
+            return UsuarioDAO.editarUsuario(usuario);
+        }
+
+        public Boolean EliminarUsuario(int idUsuario)
+        {
+            return UsuarioDAO.eliminarUsuario(idUsuario);
+        }
+    }
+}
